554169f8f6aa0278cb46d40f08c0d1ad192f92ad
Silver <rhyskoerber@gmail.com>
2024-01-09 17:13:59 +1030

    e

--- script.js
+++ script.js
@@ -17,6 +17,13 @@ var players = {}
 var gamepads = {}
 var camera = {x: 0, y: 0, zoom: 1}
 
+var delta = 0
+var lastTime = 0
+var su = 0
+
+var bullets = []
+var explosions = []
+
 function toScreen(x, y) {
     return [x+canvas.width/2, y+canvas.height/2]
 }
@@ -29,6 +36,10 @@ function getButtons(lbuttons) {
         dpt: lbuttons[12].pressed, dpb: lbuttons[13].pressed, dpl: lbuttons[14].pressed, dpr: lbuttons[15].pressed,
         guide: lbuttons[16].pressed
     }
+}
+
+function createExplosion(x, y, c) {
+    explosions.push({x: x, y: y, c: c, lifetime: 0})
 }
 
 function update(timestamp) {
@@ -65,39 +76,71 @@ function update(timestamp) {
 
     for (let gamepad in gamepads) {
         if (!(gamepad in players)) {
-            players[gamepad] = {x: 0, y: 0, vx: 0, vy: 0, angle: Math.PI, trail: []}
+            players[gamepad] = {x: 0, y: 0, vx: 0, vy: 0, cooldown: 0, angle: Math.PI, trail: []}
         }
     }
 
-    let playeri = 0
+    for (let i = 0; i < bullets.length; i++) {
+        let bullet = bullets[i]
+        bullet.x += Math.sin(bullet.angle) * delta * 500
+        bullet.y += Math.cos(bullet.angle) * delta * 500
+        bullet.lifetime += delta
+        let c = hslToRgb(parseInt(bullet.id)/4 * 360, 100, 50)
+        ui.circle(...toScreen(bullet.x, bullet.y), 5*su, [c.r, c.g, c.b, 1])
+
+        if (bullet.lifetime > 3) {
+            bullets.splice(i, 1)
+            i--
+        } else {
+            for (let player in players) {
+                if (player != bullet.id && Math.sqrt((bullet.x - players[player].x)**2 + (bullet.y - players[player].y)**2) < 20*su) {
+                    let c2 = hslToRgb(parseInt(player)/4 * 360, 100, 50)
+                    createExplosion(players[player].x, players[player].y, [c2.r, c2.g, c2.b, 1])
+                    players[player].x = Math.random()*500 - 250
+                    players[player].y = Math.random()*500 - 250
+                    bullets.splice(i, 1)
+                    i--
+                    break
+                }
+            }
+        }
+    }
+
     for (let player in players) {
         if (!(player in gamepads)) {
             delete players[player]
         } else {
             let buttons = gamepads[player].buttons
+            let lbuttons = gamepads[player].lbuttons
             let axes = gamepads[player].axes
 
-            let speed = 0.1
+            players[player].cooldown -= delta
+
+            let speed = 10
             if (buttons.lt) {
                 speed *= 3
             }
-            players[player].angle -= axes[0]/25
+            players[player].angle -= axes[0]*4 * delta
             if (buttons.a) {
-                players[player].vx += Math.sin(players[player].angle) * speed
-                players[player].vy += Math.cos(players[player].angle) * speed
+                players[player].vx += Math.sin(players[player].angle) * speed * delta
+                players[player].vy += Math.cos(players[player].angle) * speed * delta
             }
             if (buttons.b) {
-                players[player].vx -= Math.sin(players[player].angle) * speed
-                players[player].vy -= Math.cos(players[player].angle) * speed
+                players[player].vx -= Math.sin(players[player].angle) * speed * delta
+                players[player].vy -= Math.cos(players[player].angle) * speed * delta
+            }
+            if (buttons.rt && players[player].cooldown <= 0) {
+                players[player].cooldown = 0.2
+                bullets.push({id: player, lifetime: 0, x: players[player].x, y: players[player].y, angle: players[player].angle})
             }
 
-            players[player].vx = lerp(players[player].vx, 0, 0.05)
-            players[player].vy = lerp(players[player].vy, 0, 0.05)
+            players[player].vx = lerp(players[player].vx, 0, 0.05*delta*100)
+            players[player].vy = lerp(players[player].vy, 0, 0.05*delta*100)
 
-            players[player].x += players[player].vx
-            players[player].y += players[player].vy
+            players[player].x += players[player].vx * delta * 100
+            players[player].y += players[player].vy * delta * 100
 
-            if (gamepads[player].lbuttons.start) {
+            if (lbuttons.start) {
                 players[player].x = 0
                 players[player].y = 0
             }
@@ -107,7 +150,7 @@ function update(timestamp) {
                 players[player].trail.splice(0, 1)
             }
 
-            let c = hslToRgb(playeri/4 * 360, 100, 50)
+            let c = hslToRgb(parseInt(player)/4 * 360, 100, 50)
 
             ctx.lineWidth = 5*su
             ctx.lineJoin = "round"
@@ -126,8 +169,6 @@ function update(timestamp) {
                 }
             }
 
-            let sc = toScreen(players[player].x, players[player].y)
-
             ctx.beginPath()
             players[player].angle -= Math.PI/2
             let s = 1
@@ -141,7 +182,17 @@ function update(timestamp) {
             ctx.fill()
             // ui.circle(...toScreen(players[player].x, players[player].y), 10*su, [c.r, c.g, c.b, 1])
 
-            playeri++
+        }
+    }
+
+    for (let i = 0; i < explosions.length; i++) {
+        explosions[i].lifetime += delta
+        if (Math.sin(explosions[i].lifetime*Math.PI)*50*su > 0) {
+            ui.circle(...toScreen(explosions[i].x, explosions[i].y),Math.sin(explosions[i].lifetime*Math.PI)*50*su, explosions[i].c)
+        }
+        if (explosions[i].lifetime > 1) {
+            explosions.splice(i, 1)
+            i--
         }
     }
 
